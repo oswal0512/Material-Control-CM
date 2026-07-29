a6423e198e9886acd206f3cd5ab29dc5afc87a8f
Oswal0512 <oswaldora11@gmail.com>
2026-07-28 21:17:22 -0500

    Configurar archivos estáticos

--- config/settings.py
+++ config/settings.py
@@ -142,9 +142,14 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-STATICFILES_STORAGE = (
-    "whitenoise.storage.CompressedManifestStaticFilesStorage"
-)
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}   
 
 # =====================================
 # LOGIN
