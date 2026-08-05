2a8a479cd1f6488ec1ca425f233083c63828b9ad
Oswal0512 <oswaldora11@gmail.com>
2026-08-05 13:57:10 -0500

    Corregir ubicación de Select2 en base.html

--- movements/forms.py
+++ movements/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from materials.models import Material
 from .models import Delivery, DeliveryDetail
 
 
@@ -22,3 +23,28 @@ class DeliveryDetailForm(forms.ModelForm):
             "material",
             "cantidad",
         ]
+
+        widgets = {
+            "material": forms.Select(
+                attrs={
+                    "class": "form-select select2",
+                }
+            ),
+            "cantidad": forms.NumberInput(
+                attrs={
+                    "class": "form-control",
+                }
+            ),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields["material"].queryset = Material.objects.filter(
+            activo=True
+        ).order_by("codigo")
+
+        self.fields["material"].label_from_instance = (
+            lambda obj: f"{obj.codigo} | {obj.nombre}"
+        )
+        
