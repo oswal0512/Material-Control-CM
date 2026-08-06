213e0326e51b14d7f608f7803759b3bfb45cca36
Oswal0512 <oswaldora11@gmail.com>
2026-08-06 10:19:24 -0500

    Agregar búsqueda de materiales en recepciones

--- inventory/forms.py
+++ inventory/forms.py
@@ -1,5 +1,8 @@
 from django import forms
 from .models import Receipt, ReceiptDetail
+from django import forms
+from .models import Receipt, ReceiptDetail
+from materials.models import Material
 
 
 class ReceiptForm(forms.ModelForm):
@@ -13,6 +16,12 @@ class ReceiptForm(forms.ModelForm):
             "fecha",
         ]
 
+class ReceiptForm(forms.ModelForm):
+
+    class Meta:
+        model = Receipt
+        fields = "__all__"
+
 
 class ReceiptDetailForm(forms.ModelForm):
 
@@ -21,5 +30,28 @@ class ReceiptDetailForm(forms.ModelForm):
         fields = [
             "material",
             "cantidad",
-            "observacion",
         ]
+
+        widgets = {
+            "material": forms.Select(
+                attrs={
+                    "class": "form-select select2",
+                }
+            ),
+            "cantidad": forms.NumberInput(
+                attrs={
+                    "class": "form-control",
+                }
+            ),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields["material"].queryset = Material.objects.filter(
+            activo=True
+        ).order_by("codigo")
+
+        self.fields["material"].label_from_instance = (
+            lambda obj: f"{obj.codigo} | {obj.nombre}"
+        )
